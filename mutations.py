a707a2129eb080d0eac97a727e6f29d155086f0d
Stephan <31624652+stephandooper@users.noreply.github.com>
2018-06-08 16:21:07 +0200

    Edit filter mutations
    editing filter mutations in convulations

--- mutations.py
+++ mutations.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#Delete keras, default_init_nn_repr
 
-from representation import INSERTABLE, MUTABLE_PARAMS
+
+from representations import INSERTABLE, MUTABLE_PARAMS, default_init_nn_repr
 import numpy as np
+import random
+
+FILTER_MUTATIONS = np.array([16, 32, 64])
 
 
-def mutate_param(r, param):
+
+def mutate_filter(r, param):
     # TODO: implement constraints here
-    r[param] = r[param]
+    new_param = random.choice(FILTER_MUTATIONS[FILTER_MUTATIONS !=r['params'][param]])
+    r['params'][param] = new_param
     return r
 
+def mutate_droprate(r,param):
+    '''
+    My niggas, this is the function that edits dropout rates
+    This shit is so heavy no kush will make this light y'all
+    
+    r:= representation of the layer of the network
+    param:= dropout rate to be changed.
+    '''
+    new_param = random.choice(FILTER_MUTATIONS[FILTER_MUTATIONS !=r['params'][param]])
+    r['params'][param] = new_param
+    return r
+    
 
 def mutate_random_param(reprs):
+    
+    '''
+    main function to the mutate random parameters for a representation  reprs
+    
+    '''
     # Find indices of layers with mutable params
     idxs = []
     for i in range(len(reprs)):
@@ -21,11 +45,24 @@ def mutate_random_param(reprs):
     idx = np.random.choice(idxs)
     # Pick a random param
     param = np.random.choice(MUTABLE_PARAMS[reprs[idx]['type']])
-    r = mutate_param(reprs[idx], param)
+    print('param is equal to', param)
+    print('repr before is equal to',reprs[idx]['params'][param])
+    r = mutate_filter(reprs[idx], param)
+    
     reprs[idx] = r
+    print('repr after is equal to',reprs[idx]['params'][param])
     return reprs
 
 
 def insert_layer(reprs):
     # TODO: implement
     pass
+
+
+def main():
+    z=default_init_nn_repr()
+    print(z)
+    y = mutate_random_param(z)
+
+if __name__ == "__main__":
+    main()
